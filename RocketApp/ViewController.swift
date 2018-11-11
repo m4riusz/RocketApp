b7e5813f279e4b846c494e2cd7883ad2cd234431
m4riusz <sariuszm@gmail.com>
2018-11-11 18:47:22 +0100

    Initial Commit

---
+++ RocketApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RocketApp
+//
+//  Created by Mariusz Sut on 11/11/2018.
+//  Copyright © 2018 Mariusz Sut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
